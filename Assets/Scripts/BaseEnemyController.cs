f7307fcdb13e84c873fb3c345147f6aa62b0acc8
njstein19 <159060796+mrmonches@users.noreply.github.com>
2024-08-29 18:39:01 -0500

    Feature Commit: Enemy Death + Animation

--- Assets/Scripts/BaseEnemyController.cs
+++ Assets/Scripts/BaseEnemyController.cs
@@ -1,11 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class BaseEnemyController : MonoBehaviour
 {
     [SerializeField] private int EnemyHealth;
     [SerializeField] private float EnemySpeed;
+    [SerializeField] private float StaggerTimer;
 
     [SerializeField] private int EnemyValue;
+
+    private bool isDead;
+
+    private Animator _animator;
 
     private GameManager _gameManager;
 
@@ -14,6 +20,10 @@ public class BaseEnemyController : MonoBehaviour
     private void Awake()
     {
         _rb2d = GetComponent<Rigidbody2D>();
+
+        _animator = GetComponent<Animator>();
+
+        isDead = false;
 
         _rb2d.velocity = new Vector2(-EnemySpeed, 0.0f);
 
@@ -26,15 +36,50 @@ public class BaseEnemyController : MonoBehaviour
 
         if (EnemyHealth <= 0)
         {
-            _gameManager.UpdateScore(EnemyValue);
-            Destroy(gameObject);
+            isDead = true;
+
+            StartCoroutine(KillDelay());
+        }
+        else
+        {
+            _animator.SetTrigger("IsHit");
+
+            StartCoroutine(DamageStagger());
+        }
+    }
+
+    private IEnumerator DamageStagger()
+    {
+        if (true)
+        {
+            _rb2d.velocity = Vector2.zero;
+
+            yield return new WaitForSeconds(StaggerTimer);
+
+            _rb2d.velocity = new Vector2(-EnemySpeed, 0.0f);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Barrier"))
+        if ((collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Barrier")) &&  !isDead)
         {
+            Destroy(gameObject);
+        }
+    }
+
+    private IEnumerator KillDelay()
+    {
+        if (true)
+        {
+            _rb2d.velocity = Vector2.zero;
+
+            _animator.SetTrigger("IsKilled");
+
+            yield return new WaitForSeconds(StaggerTimer);
+
+            _gameManager.UpdateScore(EnemyValue);
+
             Destroy(gameObject);
         }
     }
